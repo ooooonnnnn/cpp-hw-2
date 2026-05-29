61a0cfb4e6af266c02321bc4e210f15836b70cad
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-05-29 13:04:11 +0300

    +, -, *=, /=, ==

---
+++ DynamicIntArray/DynamicIntArray/Vector2D.cpp
@@ -0,0 +1,36 @@
+﻿#include "Vector2D.h"
+
+Vector2D::Vector2D(float x, float y) : x(x), y(y)
+{
+}
+
+Vector2D Vector2D::operator+(const Vector2D& other) const
+{
+    return {x + other.x, y + other.y};
+}
+
+Vector2D Vector2D::operator-(const Vector2D& other) const
+{
+    return {x - other.x, y - other.y};
+}
+
+void Vector2D::operator*=(const float factor)
+{
+    this->x *= factor;
+    this->y *= factor;
+}
+
+void Vector2D::operator/=(const float factor)
+{
+    if (factor == 0) throw std::exception("Can't divide by 0");
+    float invFactor = 1 / factor;
+    this->x *= invFactor;
+    this->y *= invFactor;
+}
+
+bool Vector2D::operator==(const Vector2D& other) const
+{
+    float epsilon = std::numeric_limits<float>::epsilon();
+    return std::abs(x - other.x) < epsilon
+    && std::abs(y - other.y) < epsilon;
+}
---
+++ DynamicIntArray/DynamicIntArray/Vector2D.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <set>
+#include <tuple>
+
+class Vector2D
+{
+    float x;
+    float y;
+public:
+    Vector2D(float x, float y);
+    Vector2D operator+(const Vector2D& other) const;
+    Vector2D operator-(const Vector2D& other) const;
+    void operator*=(const float factor);
+    void operator/=(const float factor);
+    bool operator==(const Vector2D& other) const;
+};
